bf591f64006a53ed7d8357ea6f40603ba4436466
exITproger <200894937+exITproger@users.noreply.github.com>
2025-05-06 12:40:01 +0300

    пример с функцией Sym

---
+++ ProgramTests/ProgramTests.cs
@@ -0,0 +1,69 @@
+using Microsoft.VisualStudio.TestPlatform.TestHost;
+
+namespace ProgramTests
+{
+    [TestClass]
+    public class ProgramTests
+    {
+        [TestMethod]
+        public void Sym_ShouldReturnCorrectSum()
+        {
+            // Arrange
+            int x = 5;
+            int y = 10;
+            int expected = 15;
+
+            // Act
+            int actual = Program.Sym(x, y);
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void Sym_ShouldHandleNegativeNumbers()
+        {
+            // Arrange
+            int x = -5;
+            int y = -10;
+            int expected = -15;
+
+            // Act
+            int actual = Program.Sym(x, y);
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void Sym_ShouldHandleZero()
+        {
+            // Arrange
+            int x = 0;
+            int y = 0;
+            int expected = 0;
+
+            // Act
+            int actual = Program.Sym(x, y);
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void Sym_ShouldHandleLargeNumbers()
+        {
+            // Arrange
+            int x = int.MaxValue;
+            int y = 1;
+            // Note: This will cause overflow, which is expected behavior for this function
+            int expected = int.MinValue; // because of integer overflow
+
+            // Act
+            int actual = Program.Sym(x, y);
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
---
+++ tests/Program.cs
@@ -0,0 +1,46 @@
+﻿
+
+public class Program
+{
+    static public int Sym(int x, int y)
+    {
+        return x + y;
+    }
+    private static void Main(string[] args)
+    {
+        int a = int.Parse(Console.ReadLine());
+        int b = int.Parse(Console.ReadLine());
+        int Symma = Sym(a, b);
+        Console.WriteLine(Symma);
+        int[] s = ComputeLPSArray("аабдааабдд");
+        Console.WriteLine(String.Join(" ", s));
+    }
+    static int[] ComputeLPSArray(string pattern)
+    {
+        int[] lps = new int[pattern.Length];
+        int len = 0;
+        int i = 1;
+        while (i < pattern.Length)
+        {
+            if (pattern[i] == pattern[len])
+            {
+                len++;
+                lps[i] = len;
+                i++;
+            }
+            else
+            {
+                if (len != 0)
+                {
+                    len = lps[len - 1];
+                }
+                else
+                {
+                    lps[i] = 0;
+                    i++;
+                }
+            }
+        }
+        return lps;
+    }
+}
