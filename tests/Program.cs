cd76c3e8d1f16cf9276a4ea0ab12d66fca24340c
exITproger <204976535+sdnl000@users.noreply.github.com>
2025-05-06 21:27:14 +0300

    #3 close

--- tests/Program.cs
+++ tests/Program.cs
@@ -1,4 +1,24 @@
-﻿
+﻿public static class ComparisonUtils
+{
+    public static T Max<T>(T first, T second) where T : IComparable<T>
+    {
+        return first.CompareTo(second) >= 0 ? first : second;
+    }
+
+    public static T Max<T>(params T[] values) where T : IComparable<T>
+    {
+        if (values == null || values.Length == 0)
+            throw new ArgumentException("At least one value must be provided");
+
+        T max = values[0];
+        for (int i = 1; i < values.Length; i++)
+        {
+            if (values[i].CompareTo(max) > 0)
+                max = values[i];
+        }
+        return max;
+    }
+}
 
 public class Program
 {
